d583dcbaf818231831693eb914d9e57775967e24
Justin Donaldson <donaldson@bigml.com>
2012-01-18 18:23:43 -0800

    keeping consistent tab indents, adding the dataf functions for relational mapping

--- src/js/d3/D3.hx
+++ src/js/d3/D3.hx
@@ -4,13 +4,13 @@ import js.Dom;
 @:native("d3")
 extern class D3
 {
-    @:overload(function(selector:HtmlDom):UnboundSelection{})
-    public static function select(selector:String):UnboundSelection;
-    
-    @:overload(function(selector:Array<HtmlDom>):UnboundSelection{})
-    public static function selectAll(selector:String):UnboundSelection;
-    
-    public static var event:Event;
+	@:overload(function(selector:HtmlDom):UnboundSelection{})
+	public static function select(selector:String):UnboundSelection;
+	
+	@:overload(function(selector:Array<HtmlDom>):UnboundSelection{})
+	public static function selectAll(selector:String):UnboundSelection;
+	
+	public static var event:Event;
 	
 	/* sorting helpers */
 	public static function ascending<T>(a:T, b:T):Int;
@@ -54,155 +54,159 @@ extern class Nest<A,B>{
 
 
 extern class Selection<T> extends BaseSelection{
-    
-    @:overload(function(name:String, value:String):Selection<T>{})  
-    @:overload(function(name:String, value:Void->String):Selection<T>{})  
-    @:overload(function(name:String, value:Dynamic->String):Selection<T>{})  
-    @:overload(function(name:String, value:Dynamic->Int->String):Selection<T>{})  
-    public function attr(name:String, value:String):Selection<T>;
+	
+	@:overload(function(name:String, value:String):Selection<T>{})  
+	@:overload(function(name:String, value:Void->String):Selection<T>{})  
+	@:overload(function(name:String, value:Dynamic->String):Selection<T>{})  
+	@:overload(function(name:String, value:Dynamic->Int->String):Selection<T>{})  
+	public function attr(name:String, value:String):Selection<T>;
 
-    @:overload(function(name:String, value:String):Selection<T>{})  
-    @:overload(function(name:String, value:Void->String):Selection<T>{})  
-    @:overload(function(name:String, value:Dynamic->String):Selection<T>{})  
-    @:overload(function(name:String, value:Dynamic->Int->String):Selection<T>{})  
-    public function classed(name:String, value:String):Selection<T>;
+	@:overload(function(name:String, value:String):Selection<T>{})  
+	@:overload(function(name:String, value:Void->String):Selection<T>{})  
+	@:overload(function(name:String, value:Dynamic->String):Selection<T>{})  
+	@:overload(function(name:String, value:Dynamic->Int->String):Selection<T>{})  
+	public function classed(name:String, value:String):Selection<T>;
 
-    @:overload(function(name:String, value:String):Selection<T>{})  
-    @:overload(function(name:String, value:Void->String):Selection<T>{})  
-    @:overload(function(name:String, value:Dynamic->String):Selection<T>{})  
-    @:overload(function(name:String, value:Dynamic->Int->String):Selection<T>{})  
-    public function property(name:String, value:String):Selection<T>;
+	@:overload(function(name:String, value:String):Selection<T>{})  
+	@:overload(function(name:String, value:Void->String):Selection<T>{})  
+	@:overload(function(name:String, value:Dynamic->String):Selection<T>{})  
+	@:overload(function(name:String, value:Dynamic->Int->String):Selection<T>{})  
+	public function property(name:String, value:String):Selection<T>;
 
-    @:overload(function(name:String, value:String, ?priority:String):Selection<T>{})  
-    @:overload(function(name:String, value:Void->String, ?priority:String):Selection<T>{})  
-    @:overload(function(name:String, value:Dynamic->String, ?priority:String):Selection<T>{})  
-    @:overload(function(name:String, value:Dynamic->Int->String, ?priority:String):Selection<T>{})  
-    public function style(name:String, value:String):Selection<T>;
+	@:overload(function(name:String, value:String, ?priority:String):Selection<T>{})  
+	@:overload(function(name:String, value:Void->String, ?priority:String):Selection<T>{})  
+	@:overload(function(name:String, value:Dynamic->String, ?priority:String):Selection<T>{})  
+	@:overload(function(name:String, value:Dynamic->Int->String, ?priority:String):Selection<T>{})  
+	public function style(name:String, value:String):Selection<T>;
 
-    @:overload(function():String{})  
-    @:overload(function(value:Void->String):Selection<T>{})  
-    @:overload(function(value:Dynamic->String):Selection<T>{})  
-    @:overload(function(value:Dynamic->Int->String):Selection<T>{})
-    public function text(value:String) : Selection<T>;
-    
-    @:overload(function():String<T>{})  
-    @:overload(function(value:Void->String):Selection<T>{})  
-    @:overload(function(value:Dynamic->String):Selection<T>{})  
-    @:overload(function(value:Dynamic->Int->String):Selection<T>{})
-    public function html(value:String):Selection<T>;
-    
+	@:overload(function():String{})  
+	@:overload(function(value:Void->String):Selection<T>{})  
+	@:overload(function(value:Dynamic->String):Selection<T>{})  
+	@:overload(function(value:Dynamic->Int->String):Selection<T>{})
+	public function text(value:String) : Selection<T>;
+	
+	@:overload(function():String<T>{})  
+	@:overload(function(value:Void->String):Selection<T>{})  
+	@:overload(function(value:Dynamic->String):Selection<T>{})  
+	@:overload(function(value:Dynamic->Int->String):Selection<T>{})
+	public function html(value:String):Selection<T>;
+	
 
-    public function remove() : Selection<T>;
-    
-    public function data<A>(data:Array<A>):PreEnterSelection<A>;
-    
-    @:overload(function(fn:T->Int->A):Selection<A>{})
-    public function map<A>(fn:T->A):Selection<A>;
-    
-    public function sort(comparator:T->T->Int):Selection<T>;
-    public function order():Selection<T>;    
-    
-    @:overload(function(type:String, listener:T->Int->Void):Selection<T>{})
-    @:overload(function(type:String, listener:Void->Void):Selection<T>{})    
-    public function on(type:String, listener:T->Void): Selection<T>;
-    
-    public function transition(): Selection<T>;
-    
-    public function select(selector:String):Selection<T>;
-    public function selectAll(selector:String):Selection<T>;
-    
-    @:overload(function(fn:T->Int->Void):Selection<T>{})
-    public function each(fn:T->Void):Selection<T>;
-    
-    @:overload(function(fn:Void->Dynamic):Selection<T>{})
-    @:overload(function(fn:T->Int->Dynamic):Selection<T>{})
-    public function call(fn:T->Dynamic) : Selection<T>;
+	public function remove() : Selection<T>;
+	
+ 
+	@:overload(function<A>(?dataf:T->Array<A>):PreEnterSelection<A>{})	
+	@:overload(function<A>(?dataf:T->Int->Array<A>):PreEnterSelection<A>{})
+	@:overload(function(data:Array<A>,?key:A->Int->Dynamic):PreEnterSelection<A>{})  
+	public function data<A>(data:Array<A>, ?key:A->Dynamic):PreEnterSelection<A>;
+	
+	@:overload(function(fn:T->Int->A):Selection<A>{})
+	public function map<A>(fn:T->A):Selection<A>;
+	
+	public function sort(comparator:T->T->Int):Selection<T>;
+	public function order():Selection<T>;	
+	
+	@:overload(function(type:String, listener:T->Int->Void):Selection<T>{})
+	@:overload(function(type:String, listener:Void->Void):Selection<T>{})	
+	public function on(type:String, listener:T->Void): Selection<T>;
+	
+	public function transition(): Selection<T>;
+	
+	public function select(selector:String):Selection<T>;
+	public function selectAll(selector:String):Selection<T>;
+	
+	@:overload(function(fn:T->Int->Void):Selection<T>{})
+	public function each(fn:T->Void):Selection<T>;
+	
+	@:overload(function(fn:Void->Dynamic):Selection<T>{})
+	@:overload(function(fn:T->Int->Dynamic):Selection<T>{})
+	public function call(fn:T->Dynamic) : Selection<T>;
 
-    @:overload(function(selector:T->Int->Bool):Selection<T>{})
-    @:overload(function(selector:T->Bool):Selection<T>{})
+	@:overload(function(selector:T->Int->Bool):Selection<T>{})
+	@:overload(function(selector:T->Bool):Selection<T>{})
 	public function filter(selector:String):Selection<T>;
 }
 
 extern class UnboundSelection extends BaseSelection{
-    
-    @:overload(function(name:String, value:String):UnboundSelection{})  
-    @:overload(function(name:String, value:Void->String):UnboundSelection{})  
-    @:overload(function(name:String, value:Dynamic->String):UnboundSelection{})  
-    @:overload(function(name:String, value:Dynamic->Int->String):UnboundSelection{})  
-    public function attr(name:String, value:String):UnboundSelection;
+	
+	@:overload(function(name:String, value:String):UnboundSelection{})  
+	@:overload(function(name:String, value:Void->String):UnboundSelection{})  
+	@:overload(function(name:String, value:Dynamic->String):UnboundSelection{})  
+	@:overload(function(name:String, value:Dynamic->Int->String):UnboundSelection{})  
+	public function attr(name:String, value:String):UnboundSelection;
 
-    @:overload(function(name:String, value:String):UnboundSelection{})  
-    @:overload(function(name:String, value:Void->String):UnboundSelection{})  
-    @:overload(function(name:String, value:Dynamic->String):UnboundSelection{})  
-    @:overload(function(name:String, value:Dynamic->Int->String):UnboundSelection{})  
-    public function classed(name:String, value:String):UnboundSelection;
+	@:overload(function(name:String, value:String):UnboundSelection{})  
+	@:overload(function(name:String, value:Void->String):UnboundSelection{})  
+	@:overload(function(name:String, value:Dynamic->String):UnboundSelection{})  
+	@:overload(function(name:String, value:Dynamic->Int->String):UnboundSelection{})  
+	public function classed(name:String, value:String):UnboundSelection;
 
-    @:overload(function(name:String, value:String):UnboundSelection{})  
-    @:overload(function(name:String, value:Void->String):UnboundSelection{})  
-    @:overload(function(name:String, value:Dynamic->String):UnboundSelection{})  
-    @:overload(function(name:String, value:Dynamic->Int->String):UnboundSelection{})  
-    public function property(name:String, value:String):UnboundSelection;
+	@:overload(function(name:String, value:String):UnboundSelection{})  
+	@:overload(function(name:String, value:Void->String):UnboundSelection{})  
+	@:overload(function(name:String, value:Dynamic->String):UnboundSelection{})  
+	@:overload(function(name:String, value:Dynamic->Int->String):UnboundSelection{})  
+	public function property(name:String, value:String):UnboundSelection;
 
-    @:overload(function(name:String, value:Void->String, ?priority:Priority):UnboundSelection{})  
-    @:overload(function(name:String, value:Dynamic->String, ?priority:Priority):UnboundSelection{})  
-    @:overload(function(name:String, value:Dynamic->Int->String, ?priority:Priority):UnboundSelection{})  
-    public function style(name:String, value:String, ?priority:Priority):UnboundSelection;
+	@:overload(function(name:String, value:Void->String, ?priority:Priority):UnboundSelection{})  
+	@:overload(function(name:String, value:Dynamic->String, ?priority:Priority):UnboundSelection{})  
+	@:overload(function(name:String, value:Dynamic->Int->String, ?priority:Priority):UnboundSelection{})  
+	public function style(name:String, value:String, ?priority:Priority):UnboundSelection;
 
-    @:overload(function():String{})  
-    @:overload(function(value:Void->String):UnboundSelection{})  
-    @:overload(function(value:Dynamic->String):UnboundSelection{})  
-    @:overload(function(value:Dynamic->Int->String):UnboundSelection{})
-    public function text(value:String) : UnboundSelection;
-    
-    @:overload(function():String<T>{})  
-    @:overload(function(value:Void->String):UnboundSelection{})  
-    @:overload(function(value:Dynamic->String):UnboundSelection{})  
-    @:overload(function(value:Dynamic->Int->String):UnboundSelection{})
-    public function html(value:String):UnboundSelection;
-    
-    public function append(name:String) : UnboundSelection;
-    public function insert(name:String, ?before:String) : UnboundSelection;
-    public function remove() : UnboundSelection;
-    
+	@:overload(function():String{})  
+	@:overload(function(value:Void->String):UnboundSelection{})  
+	@:overload(function(value:Dynamic->String):UnboundSelection{})  
+	@:overload(function(value:Dynamic->Int->String):UnboundSelection{})
+	public function text(value:String) : UnboundSelection;
+	
+	@:overload(function():String<T>{})  
+	@:overload(function(value:Void->String):UnboundSelection{})  
+	@:overload(function(value:Dynamic->String):UnboundSelection{})  
+	@:overload(function(value:Dynamic->Int->String):UnboundSelection{})
+	public function html(value:String):UnboundSelection;
+	
+	public function append(name:String) : UnboundSelection;
+	public function insert(name:String, ?before:String) : UnboundSelection;
+	public function remove() : UnboundSelection;
+	
+	@:overload(function(data:Array<T>,?key:T->Int->Dynamic):PreEnterSelection<T>{})
+	public function data<T>(data:Array<T>,?key:T->Dynamic):PreEnterSelection<T>;
+	
+	public function order():UnboundSelection;
+	
 
-    public function data<T>(data:Array<T>):PreEnterSelection<T>;
-    
-    public function order():UnboundSelection;
-    
+	public function on(type:String, listener:Dynamic->Void): UnboundSelection;
+	
+	public function transition(): UnboundSelection;
+	public function select(selector:String):UnboundSelection;
+	public function selectAll(selector:String):UnboundSelection;
 
-    public function on(type:String, listener:Dynamic->Void): UnboundSelection;
-    
-    public function transition(): UnboundSelection;
-    public function select(selector:String):UnboundSelection;
-    public function selectAll(selector:String):UnboundSelection;
-
-    @:overload(function(fn:Void->Dynamic):UnboundSelection{})    
-    @:overload(function(fn:Dynamic->Int->Dynamic):UnboundSelection{})
-    public function call(fn:Dynamic->Dynamic) : UnboundSelection;
-    
+	@:overload(function(fn:Void->Dynamic):UnboundSelection{})	
+	@:overload(function(fn:Dynamic->Int->Dynamic):UnboundSelection{})
+	public function call(fn:Dynamic->Dynamic) : UnboundSelection;
+	
 }
 
 extern class PreEnterSelection<T>{
-    public function enter():EnterSelection<T>;
-    public function exit():Selection<T>;
+	public function enter():EnterSelection<T>;
+	public function exit():Selection<T>;
 }
 
 extern class EnterSelection<T>{
-    public function append(name:String) : Selection<T>;
-    public function insert(name:String, ?before:String) : Selection<T>;
+	public function append(name:String) : Selection<T>;
+	public function insert(name:String, ?before:String) : Selection<T>;
 }
 
 extern class BaseSelection extends D3Array<HtmlDom>{
-    public function empty():Bool;
-    public function node():HtmlDom;    
+	public function empty():Bool;
+	public function node():HtmlDom;	
 }
 
 extern enum Priority{
-    important;    
+	important;	
 }
 
 class InitPriority{
-    static var init = {untyped __js__('Priority = { important : "important"};');} 
+	static var init = {untyped __js__('Priority = { important : "important"};');} 
 }
 
 extern class D3Array<T> implements ArrayAccess<T>{
